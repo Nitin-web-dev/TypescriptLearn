e6535088472db58eb2704643e280d3eab2382d2d
Nitin Singh <Nitinwebdev5@gmail.com>
2023-09-19 15:28:28 +0530

    addead lession07 tp= function return optional default void

--- app.ts
+++
@@ -1,3 +0,0 @@
-// this is file to check if file outside src will be genearte or note 
-
-// include tsconfig.json include: ['src']
--- public/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-console.log('this is test file 123456');
-// you can use tsc -w 
-// to watch all changes directly not use tsc everytime -w watch will do it autometically
-// there will be an error if we crated a file outside the src it will directly generate the file for it in public but we don't want it so we gona make changes in tsconfig.json file => atjust type it if not found in the file =>  include: [src]

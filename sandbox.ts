e5c4bd8441ca6c0a4a055e13ec3fe1a9e56a3a84
Nitin Singh <Nitinwebdev5@gmail.com>
2023-09-19 14:26:04 +0530

    addead lession03 array and object

--- sandbox.ts
+++ sandbox.ts
@@ -1,21 +1,43 @@
-let character = 'mario'
-let age = 20
-let isBlackbelt = false
+// array
+let names = ["sam", "sumit", "rahul"];
+
+names.push("tosad");
+// names.push(3) // error bcos array is only stirng
+// names[0] = 3 //error
+
+let numbers = [10, 12, 213];
+
+numbers.push(23);
+// numbers.push('sag')// error for same
+// numbers[0] = 'asdffs'
+
+// but we can crate mixed
+let mixed = ["sam", 20, "rahul", 234];
+
+mixed.push("samsingh");
+mixed.push(234);
+mixed[2] = "aksh";
+
+// objects
+
+let ninjs = {
+  name: "mario",
+  belt: "black",
+  age: 30,
+};
+
+ninjs.age = 40;
+ninjs.name = 'sam'
+// ninjs.age = '30'; // error same reason type change
+// ninjs.skills = ['fighting', 'sneaking'] // error
 
 
-// character = 20 // error bcox it string type
-character = 'luigi'
+// we can overide which only exist
 
-
-//age = 'sasd' // error for same reasone
-age = 40
-
-
-// same goes for isBackbelt
-
-
-const circ = (diameter:number):number =>{
-    return diameter * Math.PI;
+ninjs = {
+    name: 'yoshi',
+    belt : 'oragnge',
+    // age: '34' // error type 
+    age: 34,
+    // skills : ['sdfasdf','asdfasdfsf'] // error bcos it does n't exits in original
 }
-
-console.log(circ(7.5))
